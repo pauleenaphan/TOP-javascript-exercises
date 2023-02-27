19a0c78aa5f3b7c6bd81785ab2390245c3a40c47
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-02-26 20:46:19 -0800

    use solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 const removeFromArray = function(arr, ...num) {
     /*
+    //idk why this doesnt work
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === num){
             arr.splice(i, 1);
@@ -8,14 +9,6 @@ const removeFromArray = function(arr, ...num) {
     
     return arr;
     */
-
-    let newArr = []
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] != num){
-            newArr.push(i);
-        }
-    }
-    return newArr;
 };
 
 // Do not edit below this line
