30d9dff1005fc22c32ec526840b778b5d09cb8ad
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-02-26 20:24:28 -0800

    switch this commit with problem 2 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    for(let i = 0; i < (str.length/2); i++){
+        /*
+        temp = str[i];
+        console.log(temp);
+        str[i] = str[str.length - i];
+        console.log(str[i]);
+        str[str.length - i] = temp;
+        */
 
+        return str.split("").reverse().join("");
+    }
+    return str;
 };
 
 // Do not edit below this line
