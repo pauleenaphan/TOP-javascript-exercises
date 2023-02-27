6c6beb98a3ef3fdcba9586d6435e38aec186b62b
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-02-26 20:23:47 -0800

    used solution my working one didn't work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, numb) {
+    if(numb < 0){
+        return("ERROR");
+    }
 
+    let strRet = "";
+    for(let i = 0; i < numb; i++){
+        strRet = str + strRet;
+    }
+    return strRet;
 };
 
 // Do not edit below this line
