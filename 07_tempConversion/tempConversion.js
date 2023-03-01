7e7f31aa51ea49a92ef5925c4a47ff7b5797e6da
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-03-01 15:21:53 -0800

    finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  value = ((num - 32) * .5556);
+  return Math.round(value * 10)/(10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  value = ((num * 1.8) + 32);
+  return Math.round(value * 10)/(10);
 };
 
 // Do not edit below this line
