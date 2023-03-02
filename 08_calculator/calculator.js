4e61bb1be59553c175dd72c05d1119e6f34121b4
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-03-01 21:50:32 -0800

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	arr.forEach(item => {
+    total += item;
+  })
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  //this cannot be 0 bc everything times 0 = 0
+  let total = 1;
 
+  for(let i = 0; i < arr.length; i++){
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 1 || num == 0){
+    return 1;
+  }else{
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
