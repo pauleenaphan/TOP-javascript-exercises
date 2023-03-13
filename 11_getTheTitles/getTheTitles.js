e6a03284d5eeed7f0195439b24a0017a10db9b24
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-03-12 17:55:13 -0700

    Finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i].title);
+        arr2[i] = arr[i].title;
+    }
+    return arr2;
 };
 
 // Do not edit below this line
