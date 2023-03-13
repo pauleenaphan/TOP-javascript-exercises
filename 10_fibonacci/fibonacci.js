8feab4386f5ce14f050ad48d9a1900e2dfdf4f98
pauleenaphan <pauleenaphan@Xubuntu.myguest.virtualbox.org>
2023-03-12 17:17:44 -0700

    not passing one test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    if(term < 1){
+        return "OOPS";  
+    }else{
+        return fibonacci(term-1) + fibonacci(term-2);
+    }
+    
 };
 
 // Do not edit below this line
